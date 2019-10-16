a4d712b9ab95419b1ddbb460e9251a43e770e17e
Shane Quick <shanesfast@gmail.com>
2019-10-16 23:41:39 +0000

    Add suppot for video uploads

--- Gemfile
+++ Gemfile
@@ -77,3 +77,4 @@ gem 'carrierwave', '~> 2.0'
 gem "figaro"
 gem "fog-aws"
 gem "mini_magick"
+gem 'carrierwave-aws'
--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -1,10 +1,10 @@
-class ImageUploader < CarrierWave::Uploader::Base
+class VideoUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
+  # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
+  #  storage :file
   storage :aws
 
   # Override the directory where uploaded files will be stored.
@@ -27,8 +27,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def scale(width, height)
   #   # do something
   # end
-
-  process resize_to_fill: [800, 350]
 
   # Create different versions of your uploaded files:
   # version :thumb do
