5f4dbd4a727c4b04c8903f58e954a3db24b4b54a
Shane Quick <shanesfast@gmail.com>
2019-10-18 02:00:55 +0000

    Add authentication for enrollment in courses.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,17 @@
 class LessonsController < ApplicationController
-  def show
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course
   
+  def show
   end
 
   private
+
+  def require_authorized_for_current_course
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: "You are not yet enrolled in that course."
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
